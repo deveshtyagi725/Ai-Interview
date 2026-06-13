d1de0f172d0001a406d5cc7b2f3577b94438038c
deveshtyagi735 <tyagidevesh809@gmail.com>
2026-06-13 21:31:23 +0530

    Try chrome channel

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -69,27 +69,25 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 }
 
 async function generatePdfFromHtml(htmlContent) {
-    console.log("Chrome path:", puppeteer.executablePath())
-
     const browser = await puppeteer.launch({
-        executablePath: puppeteer.executablePath(),
+        channel: "chrome",
         headless: true,
         args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
+            "--no-sandbox",
+            "--disable-setuid-sandbox"
         ]
     })
 
     const page = await browser.newPage()
-    await page.setContent(htmlContent, { waitUntil: 'networkidle0' })
+    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
     const pdfBuffer = await page.pdf({
-        format: 'A4',
+        format: "A4",
         margin: {
-            top: '20mm',
-            bottom: '20mm',
-            left: '15mm',
-            right: '15mm'
+            top: "20mm",
+            bottom: "20mm",
+            left: "15mm",
+            right: "15mm"
         }
     })
 
